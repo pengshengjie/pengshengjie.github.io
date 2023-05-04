f3902ef10fb0153bb2697f2e2b8b0ec5759bafbb
PengShengjieDev <speng@fintechautomation.com>
2023-05-04 14:39:22 +0800

    ss

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "CodingJ的博客",
-  tagline: "Dinosaurs are cool",
+  tagline: "The codingJ Blog",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
